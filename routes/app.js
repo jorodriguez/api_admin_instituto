7face0e0417b46e8293bdbceeafb1dff86376bb0
joel <joel.rod.roj@hotmail.com>
2022-02-28 12:43:36 -0600

    trabajando con la baja del alumno

--- routes/app.js
+++ routes/app.js
@@ -5,7 +5,7 @@ const { pool } = require('../db/conexion');
 const port = process.env.PORT || 5000;
 
 //version/branch
-const version = "v1/220212-corecciones";
+const version = "v1/220225-hotfix-baja-alumno";
 
 
 app.use(bodyParser.json());
