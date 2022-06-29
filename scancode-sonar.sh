e833bb647a2c17ad464230bd6b2ec20cc069e7cb
= <=>
2022-06-28 22:05:17 -0500

    trabajando con el alta del curso

---
+++ scancode-sonar.sh
@@ -0,0 +1,5 @@
+/home/joel/Documentos/desarrollo/sonarqube/sonar-scanner-cli-4.7.0.2747-linux/sonar-scanner-4.7.0.2747-linux/bin/sonar-scanner \
+  -Dsonar.projectKey=api-admin-instituto-local \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.login=sqp_71c2628c1ebc3db33e8ea743f98a9923eec5bb20
