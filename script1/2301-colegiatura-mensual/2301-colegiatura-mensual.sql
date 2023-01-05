bf0491c34e62960194940be22617d693e07604ad
= <=>
2023-01-04 22:33:13 -0600

    trabajando con la inscripcion

--- script1/2301-colegiatura-mensual/2301-colegiatura-mensual.sql
+++ script1/2301-colegiatura-mensual/2301-colegiatura-mensual.sql
@@ -14,9 +14,12 @@ insert into cat_esquema_pago(id,nombre, genero) values(1,'Semanal',1),(2,'Mensua
 
 alter table co_inscripcion add column cat_esquema_pago integer not null default 1 references cat_esquema_pago(id);
 
-alter table co_cargo_balance_alumno add column cat_esquema_pago integer not null default 1 references co_cargo_balance_alumno(id);
+alter table co_cargo_balance_alumno add column cat_esquema_pago integer references co_cargo_balance_alumno(id);
 
+update co_cargo_balance_alumno set cat_esquema_pago = 1;
 
 alter table co_inscripcion alter column cat_esquema_pago drop default;
 
 alter table co_cargo_balance_alumno alter column cat_esquema_pago drop default;
+
+alter table co_cargo_balance_alumno alter column cat_esquema_pago drop not null;
